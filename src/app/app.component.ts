a3b9245bcb539f68af943e8d74e5668c82b11b12
sylvainGre <sylvain.grella@gmail.com>
2019-09-26 19:47:40 +0200

    Post class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -7,24 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Posts';
-  posts = [
-    {
-      title : 'Mon premier post',
-      content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ullamcorper libero, quis aliquam mi commodo condimentum. Aenean molestie leo magna, a mollis enim laoreet in. Phasellus bibendum, mi quis tincidunt porta, metus mi porta nibh, sit amet condimentum odio nisl consequat diam. In rutrum justo ornare, blandit felis ac, vehicula leo. Nulla faucibus vel orci a eleifend. Duis eget pretium sapien. Nullam sed neque tellus',
-      loveIts : 0,
-      created_at : new Date()
-    },
-    {
-      title : 'Mon deuxième post',
-      content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ullamcorper libero, quis aliquam mi commodo condimentum. Aenean molestie leo magna, a mollis enim laoreet in. Phasellus bibendum, mi quis tincidunt porta, metus mi porta nibh, sit amet condimentum odio nisl consequat diam. In rutrum justo ornare, blandit felis ac, vehicula leo. Nulla faucibus vel orci a eleifend. Duis eget pretium sapien. Nullam sed neque tellus',
-      loveIts : 0,
-      created_at : new Date()
-    },
-    {
-      title : 'Encore un post',
-      content : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ullamcorper libero, quis aliquam mi commodo condimentum. Aenean molestie leo magna, a mollis enim laoreet in. Phasellus bibendum, mi quis tincidunt porta, metus mi porta nibh, sit amet condimentum odio nisl consequat diam. In rutrum justo ornare, blandit felis ac, vehicula leo. Nulla faucibus vel orci a eleifend. Duis eget pretium sapien. Nullam sed neque tellus',
-      loveIts : 0,
-      created_at : new Date()
-    }
-  ]
+
+  post1 = new Post('Mon premier post','Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ullamcorper libero, quis aliquam mi commodo condimentum. Aenean molestie leo magna, a mollis enim laoreet in. Phasellus bibendum, mi quis tincidunt porta, metus mi porta nibh, sit amet condimentum odio nisl consequat diam. In rutrum justo ornare, blandit felis ac, vehicula leo. Nulla faucibus vel orci a eleifend. Duis eget pretium sapien. Nullam sed neque tellus');
+  post2 = new Post('Mon deuxième post','Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ullamcorper libero, quis aliquam mi commodo condimentum. Aenean molestie leo magna, a mollis enim laoreet in. Phasellus bibendum, mi quis tincidunt porta, metus mi porta nibh, sit amet condimentum odio nisl consequat diam. In rutrum justo ornare, blandit felis ac, vehicula leo. Nulla faucibus vel orci a eleifend. Duis eget pretium sapien. Nullam sed neque tellus');
+  post3 = new Post('Encore un post','Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus ullamcorper libero, quis aliquam mi commodo condimentum. Aenean molestie leo magna, a mollis enim laoreet in. Phasellus bibendum, mi quis tincidunt porta, metus mi porta nibh, sit amet condimentum odio nisl consequat diam. In rutrum justo ornare, blandit felis ac, vehicula leo. Nulla faucibus vel orci a eleifend. Duis eget pretium sapien. Nullam sed neque tellus');
+  
+  posts : Post[] = [this.post1, this.post2, this.post3];
+
 }
